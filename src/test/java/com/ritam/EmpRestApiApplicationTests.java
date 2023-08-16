febf58fec19ac854c02c992a3ef5f7c6cf2275ad
Ritam Ghosh <ritamghosh97@gmail.com>
2023-08-16 19:53:21 +0530

    Test class for our Rest Api

---
+++ src/test/java/com/ritam/EmpRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
