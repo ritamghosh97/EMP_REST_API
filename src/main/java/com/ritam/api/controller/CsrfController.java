01e153706ae878240f363019a71fe46b49e2ff7e
Ritam Ghosh <ritamghosh97@gmail.com>
2023-09-15 19:31:18 +0530

    Created CsrfController to retrieve csrf token.

---
+++ src/main/java/com/ritam/api/controller/CsrfController.java
@@ -0,0 +1,14 @@
+package com.ritam.api.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CsrfController {
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+}
